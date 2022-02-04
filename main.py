3887609682497886a7086960c4aebf85ad552287
Plamen Atanasov <spoder@gmail.com>
2022-02-04 20:56:08 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///to-do-list.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    to_do = db.Column(db.String(250), unique=True, nullable=False)
+
+
+db.create_all()
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    all_tasks = db.session.query(Task).all()
+    return render_template("index.html", all_tasks=all_tasks)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.form
+    new_job = Task(to_do=data["new_task"])
+    db.session.add(new_job)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
